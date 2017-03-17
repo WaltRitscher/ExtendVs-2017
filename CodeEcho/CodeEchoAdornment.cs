62ba6678f511688dc23956b8adbbc01d67ab844f
Walt Ritscher <waltritscher@hotmail.com>
2017-03-17 10:29:06 -0700

    Change UI for the WPF page.
    Rename the Layoutchanged handler

--- CodeEcho/CodeEchoAdornment.cs
+++ CodeEcho/CodeEchoAdornment.cs
@@ -12,17 +12,14 @@ using System.Windows.Controls;
 namespace CodeEcho {
 
   /// <summary>
-  /// Adornment class that draws a square box in the top right hand corner of the viewport
+  /// Adornment class that places the WPF page in upper right hand corner of the viewport
   /// </summary>
   internal class CodeEchoAdornment {
     private IWpfTextView _view;
     private IAdornmentLayer _adornmentLayer;
     private StatsPage _statsPage = new StatsPage();
 
-    /// <summary>
-    /// Creates a square image and attaches an event handler to the layout changed event that
-    /// adds the the square in the upper right-hand corner of the TextView via the adornment layer
-    /// </summary>
+ 
     /// <param name="view">The <see cref="IWpfTextView"/> upon which the adornment will be drawn</param>
     public CodeEchoAdornment(IWpfTextView view) {
       _view = view;
@@ -30,10 +27,10 @@ namespace CodeEcho {
 
       //Grab a reference to the adornment layer//
       _adornmentLayer = view.GetAdornmentLayer("CodeEchoAdornment");
-      _view.LayoutChanged += View_LayoutChanged;
+      _view.LayoutChanged += View_TextEditorLayoutChanged;
     }
 
-    private void View_LayoutChanged(object sender, TextViewLayoutChangedEventArgs e) {
+    private void View_TextEditorLayoutChanged(object sender, TextViewLayoutChangedEventArgs e) {
       //clear the adornment layer of previous adornments
       _adornmentLayer.RemoveAllAdornments();
 
@@ -99,8 +96,8 @@ namespace CodeEcho {
     }
 
     internal Language DetectLanguage(IWpfTextView view) {
-      string langtype = view.FormattedLineSource.TextAndAdornmentSequencer.
-SourceBuffer.ContentType.DisplayName;
+      string langtype = 
+                view.FormattedLineSource.TextAndAdornmentSequencer.SourceBuffer.ContentType.DisplayName;
       if (langtype.Equals("CSHARP", StringComparison.InvariantCultureIgnoreCase))
       {
         return Language.CSharp;
